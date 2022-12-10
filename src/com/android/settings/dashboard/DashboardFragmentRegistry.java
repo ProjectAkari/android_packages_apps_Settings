1e8ae5d8424b70762ce122354013fb0a5399c131
ItzKaguya <106611054+MizuNotCool@users.noreply.github.com>
2022-12-10 16:22:34 +0800

    akari: add AkariSettings

--- src/com/android/settings/dashboard/DashboardFragmentRegistry.java
+++ src/com/android/settings/dashboard/DashboardFragmentRegistry.java
@@ -1,23 +1,6 @@
-/*
- * Copyright (C) 2017 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.android.settings.dashboard;
 
 import android.util.ArrayMap;
-
 import com.android.settings.DisplaySettings;
 import com.android.settings.LegalSettings;
 import com.android.settings.accounts.AccountDashboardFragment;
@@ -46,94 +29,50 @@ import com.android.settings.security.LockscreenDashboardFragment;
 import com.android.settings.security.SecurityAdvancedSettings;
 import com.android.settings.security.SecuritySettings;
 import com.android.settings.system.SystemDashboardFragment;
-import com.android.settingslib.drawer.CategoryKey;
-
 import java.util.Map;
+import org.slimextensions.extensions.ExtensionsSettings;
 
-/**
- * A registry to keep track of which page hosts which category.
- */
+/* loaded from: classes.dex */
 public class DashboardFragmentRegistry {
-
-    /**
-     * Map from parent fragment to category key. The parent fragment hosts child with
-     * category_key.
-     */
+    public static final Map<String, String> CATEGORY_KEY_TO_PARENT_MAP;
     public static final Map<String, String> PARENT_TO_CATEGORY_KEY_MAP;
 
-    /**
-     * Map from category_key to parent. This is a helper to look up which fragment hosts the
-     * category_key.
-     */
-    public static final Map<String, String> CATEGORY_KEY_TO_PARENT_MAP;
-
     static {
-        PARENT_TO_CATEGORY_KEY_MAP = new ArrayMap<>();
-        PARENT_TO_CATEGORY_KEY_MAP.put(TopLevelSettings.class.getName(),
-                CategoryKey.CATEGORY_HOMEPAGE);
-        PARENT_TO_CATEGORY_KEY_MAP.put(
-                NetworkDashboardFragment.class.getName(), CategoryKey.CATEGORY_NETWORK);
-        PARENT_TO_CATEGORY_KEY_MAP.put(ConnectedDeviceDashboardFragment.class.getName(),
-                CategoryKey.CATEGORY_CONNECT);
-        PARENT_TO_CATEGORY_KEY_MAP.put(AdvancedConnectedDeviceDashboardFragment.class.getName(),
-                CategoryKey.CATEGORY_DEVICE);
-        PARENT_TO_CATEGORY_KEY_MAP.put(AppDashboardFragment.class.getName(),
-                CategoryKey.CATEGORY_APPS);
-        PARENT_TO_CATEGORY_KEY_MAP.put(PowerUsageSummary.class.getName(),
-                CategoryKey.CATEGORY_BATTERY);
-        PARENT_TO_CATEGORY_KEY_MAP.put(DisplaySettings.class.getName(),
-                CategoryKey.CATEGORY_DISPLAY);
-        PARENT_TO_CATEGORY_KEY_MAP.put(EmergencyDashboardFragment.class.getName(),
-                CategoryKey.CATEGORY_EMERGENCY);
-        PARENT_TO_CATEGORY_KEY_MAP.put(SoundSettings.class.getName(),
-                CategoryKey.CATEGORY_SOUND);
-        PARENT_TO_CATEGORY_KEY_MAP.put(StorageDashboardFragment.class.getName(),
-                CategoryKey.CATEGORY_STORAGE);
-        PARENT_TO_CATEGORY_KEY_MAP.put(SecuritySettings.class.getName(),
-                CategoryKey.CATEGORY_SECURITY);
-        PARENT_TO_CATEGORY_KEY_MAP.put(SecurityAdvancedSettings.class.getName(),
-                CategoryKey.CATEGORY_SECURITY_ADVANCED_SETTINGS);
-        PARENT_TO_CATEGORY_KEY_MAP.put(AccountDetailDashboardFragment.class.getName(),
-                CategoryKey.CATEGORY_ACCOUNT_DETAIL);
-        PARENT_TO_CATEGORY_KEY_MAP.put(AccountDashboardFragment.class.getName(),
-                CategoryKey.CATEGORY_ACCOUNT);
-        PARENT_TO_CATEGORY_KEY_MAP.put(
-                SystemDashboardFragment.class.getName(), CategoryKey.CATEGORY_SYSTEM);
-        PARENT_TO_CATEGORY_KEY_MAP.put(LanguageAndInputSettings.class.getName(),
-                CategoryKey.CATEGORY_SYSTEM_LANGUAGE);
-        PARENT_TO_CATEGORY_KEY_MAP.put(DevelopmentSettingsDashboardFragment.class.getName(),
-                CategoryKey.CATEGORY_SYSTEM_DEVELOPMENT);
-        PARENT_TO_CATEGORY_KEY_MAP.put(ConfigureNotificationSettings.class.getName(),
-                CategoryKey.CATEGORY_NOTIFICATIONS);
-        PARENT_TO_CATEGORY_KEY_MAP.put(LockscreenDashboardFragment.class.getName(),
-                CategoryKey.CATEGORY_SECURITY_LOCKSCREEN);
-        PARENT_TO_CATEGORY_KEY_MAP.put(ZenModeSettings.class.getName(),
-                CategoryKey.CATEGORY_DO_NOT_DISTURB);
-        PARENT_TO_CATEGORY_KEY_MAP.put(GestureSettings.class.getName(),
-                CategoryKey.CATEGORY_GESTURES);
-        PARENT_TO_CATEGORY_KEY_MAP.put(NightDisplaySettings.class.getName(),
-                CategoryKey.CATEGORY_NIGHT_DISPLAY);
-        PARENT_TO_CATEGORY_KEY_MAP.put(PrivacyDashboardFragment.class.getName(),
-                CategoryKey.CATEGORY_PRIVACY);
-        PARENT_TO_CATEGORY_KEY_MAP.put(EnterprisePrivacySettings.class.getName(),
-                CategoryKey.CATEGORY_ENTERPRISE_PRIVACY);
-        PARENT_TO_CATEGORY_KEY_MAP.put(LegalSettings.class.getName(),
-                CategoryKey.CATEGORY_ABOUT_LEGAL);
-        PARENT_TO_CATEGORY_KEY_MAP.put(MyDeviceInfoFragment.class.getName(),
-                CategoryKey.CATEGORY_MY_DEVICE_INFO);
-        PARENT_TO_CATEGORY_KEY_MAP.put(BatterySaverSettings.class.getName(),
-                CategoryKey.CATEGORY_BATTERY_SAVER_SETTINGS);
-        PARENT_TO_CATEGORY_KEY_MAP.put(SmartBatterySettings.class.getName(),
-                CategoryKey.CATEGORY_SMART_BATTERY_SETTINGS);
-
-        CATEGORY_KEY_TO_PARENT_MAP = new ArrayMap<>(PARENT_TO_CATEGORY_KEY_MAP.size());
-
-        for (Map.Entry<String, String> parentToKey : PARENT_TO_CATEGORY_KEY_MAP.entrySet()) {
-            CATEGORY_KEY_TO_PARENT_MAP.put(parentToKey.getValue(), parentToKey.getKey());
+        ArrayMap arrayMap = new ArrayMap();
+        PARENT_TO_CATEGORY_KEY_MAP = arrayMap;
+        arrayMap.put(TopLevelSettings.class.getName(), "com.android.settings.category.ia.homepage");
+        arrayMap.put(NetworkDashboardFragment.class.getName(), "com.android.settings.category.ia.wireless");
+        arrayMap.put(ConnectedDeviceDashboardFragment.class.getName(), "com.android.settings.category.ia.connect");
+        arrayMap.put(AdvancedConnectedDeviceDashboardFragment.class.getName(), "com.android.settings.category.ia.device");
+        arrayMap.put(AppDashboardFragment.class.getName(), "com.android.settings.category.ia.apps");
+        arrayMap.put(PowerUsageSummary.class.getName(), "com.android.settings.category.ia.battery");
+        arrayMap.put(DisplaySettings.class.getName(), "com.android.settings.category.ia.display");
+        arrayMap.put(EmergencyDashboardFragment.class.getName(), "com.android.settings.category.ia.emergency");
+        arrayMap.put(SoundSettings.class.getName(), "com.android.settings.category.ia.sound");
+        arrayMap.put(StorageDashboardFragment.class.getName(), "com.android.settings.category.ia.storage");
+        arrayMap.put(SecuritySettings.class.getName(), "com.android.settings.category.ia.security");
+        arrayMap.put(SecurityAdvancedSettings.class.getName(), "com.android.settings.category.ia.advanced_security");
+        arrayMap.put(AccountDetailDashboardFragment.class.getName(), "com.android.settings.category.ia.account_detail");
+        arrayMap.put(AccountDashboardFragment.class.getName(), "com.android.settings.category.ia.accounts");
+        arrayMap.put(SystemDashboardFragment.class.getName(), "com.android.settings.category.ia.system");
+        arrayMap.put(LanguageAndInputSettings.class.getName(), "com.android.settings.category.ia.language");
+        arrayMap.put(DevelopmentSettingsDashboardFragment.class.getName(), "com.android.settings.category.ia.development");
+        arrayMap.put(ConfigureNotificationSettings.class.getName(), "com.android.settings.category.ia.notifications");
+        arrayMap.put(LockscreenDashboardFragment.class.getName(), "com.android.settings.category.ia.lockscreen");
+        arrayMap.put(ZenModeSettings.class.getName(), "com.android.settings.category.ia.dnd");
+        arrayMap.put(GestureSettings.class.getName(), "com.android.settings.category.ia.gestures");
+        arrayMap.put(NightDisplaySettings.class.getName(), "com.android.settings.category.ia.night_display");
+        arrayMap.put(PrivacyDashboardFragment.class.getName(), "com.android.settings.category.ia.privacy");
+        arrayMap.put(EnterprisePrivacySettings.class.getName(), "com.android.settings.category.ia.enterprise_privacy");
+        arrayMap.put(LegalSettings.class.getName(), "com.android.settings.category.ia.about_legal");
+        arrayMap.put(MyDeviceInfoFragment.class.getName(), "com.android.settings.category.ia.my_device_info");
+        arrayMap.put(BatterySaverSettings.class.getName(), "com.android.settings.category.ia.battery_saver_settings");
+        arrayMap.put(SmartBatterySettings.class.getName(), "com.android.settings.category.ia.smart_battery_settings");
+        arrayMap.put(ExtensionsSettings.class.getName(), "com.android.settings.category.ia.extensions");
+        CATEGORY_KEY_TO_PARENT_MAP = new ArrayMap(arrayMap.size());
+        for (Map.Entry entry : arrayMap.entrySet()) {
+            CATEGORY_KEY_TO_PARENT_MAP.put((String) entry.getValue(), (String) entry.getKey());
         }
-
-        // For injection index, redirect CATEGORY_ACCOUNT_DETAIL to AccountDashboardFragment.
-        CATEGORY_KEY_TO_PARENT_MAP.put(CategoryKey.CATEGORY_ACCOUNT_DETAIL,
-                AccountDashboardFragment.class.getName());
+        CATEGORY_KEY_TO_PARENT_MAP.put("com.android.settings.category.ia.account_detail", AccountDashboardFragment.class.getName());
     }
 }
